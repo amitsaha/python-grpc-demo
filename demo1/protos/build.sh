67877398749154532b294ac72ca4b7b8555b178d
Amit Saha <amitsaha.in@gmail.com>
2017-08-27 20:39:03 +1000

    update

--- demo1/protos/build.sh
+++ demo1/protos/build.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-python -m grpc_tools.protoc \
-    --proto_path=. \
-    --python_out=../service \
-    --grpc_python_out=../service \
-    *.proto
+declare -a services=("users")
+
+# Python
+# $ pip install grpcio-tools
+
+for SERVICE in "${services[@]}"; do
+    DESTDIR='gen-py'
+    mkdir -p $DESTDIR/$SERVICE
+    touch $DESTDIR/$SERVICE/__init__.py
+    python -m grpc_tools.protoc \
+        --proto_path=$SERVICE/ \
+        --python_out=$DESTDIR/$SERVICE \
+        --grpc_python_out=$DESTDIR/$SERVICE \
+        $SERVICE/*.proto
+done
+
+# Golang
+# Install protoc
+# Install go get -a github.com/golang/protobuf/protoc-gen-go
+
+for SERVICE in "${services[@]}"; do
+    DESTDIR='gen-go'
+    mkdir -p $DESTDIR/$SERVICE
+    protoc \
+        --proto_path=$SERVICE/ \
+        --go_out=plugins=grpc:$DESTDIR/$SERVICE \
+        $SERVICE/*.proto
+done
