846ccede0103c065bad8c8781e2e34e60f535ec3
Amit Saha <amitsaha.in@gmail.com>
2017-09-05 23:33:11 +1000

    update

--- demo1/webapp/app.py
+++ demo1/webapp/app.py
@@ -8,16 +8,16 @@ import users_pb2_grpc as users_service
 import users_types_pb2 as users_messages
 
 app = Flask(__name__)
+app.config['users'] = ServiceClient(users_service, 'UsersStub', 'localhost', 50051)
 
 @app.route('/users/')
 def users_get():
-    users = ServiceClient(users_service, 'UsersStub', 'localhost', 50051)
     request = users_messages.GetUsersRequest(
         user=[users_messages.User(username="alexa", user_id=1),
               users_messages.User(username="christie", user_id=1)]
     )
     def get_user():
-        response = users.GetUsers(request)
+        response = app.config['users'].GetUsers(request)
         for resp in response:
             yield MessageToJson(resp)
     return Response(get_user(), content_type='application/json')
