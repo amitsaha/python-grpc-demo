3719b309daae3e23aaee57ae0858416dd38a7e24
Amit Saha <amitsaha.in@gmail.com>
2017-09-13 16:18:43 +1000

    Update server indentation

--- demo1/grpc-services/users/server/server.py
+++ demo1/grpc-services/users/server/server.py
@@ -11,30 +11,34 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 class UsersService(users_service.UsersServicer):
 
-  def CreateUser(self, request, context):
-      metadata = dict(context.invocation_metadata())
-      user = users_messages.User(username=request.username, user_id=1)
-      return users_messages.CreateUserResult(user=user)
+    def CreateUser(self, request, context):
+        metadata = dict(context.invocation_metadata())
+        print(metadata)
+        user = users_messages.User(username=request.username, user_id=1)
+        return users_messages.CreateUserResult(user=user)
 
-  def GetUsers(self, request, context):
-      metadata = dict(context.invocation_metadata())
-      if not metadata.get('user_id'):
-          pass
-      for user in request.user:
-          user = users_messages.User(username=user.username,
-                                     user_id=user.user_id)
-          yield users_messages.GetUsersResult(user=user)
+    def GetUsers(self, request, context):
+        metadata = dict(context.invocation_metadata())
+        if not metadata.get('user_id'):
+            pass
+        for user in request.user:
+            user = users_messages.User(
+                username=user.username, user_id=user.user_id
+            )
+            yield users_messages.GetUsersResult(user=user)
+
 
 def serve():
-  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-  users_service.add_UsersServicer_to_server(UsersService(), server)
-  res = server.add_insecure_port('127.0.0.1:50051')
-  server.start()
-  try:
-    while True:
-      time.sleep(_ONE_DAY_IN_SECONDS)
-  except KeyboardInterrupt:
-    server.stop(0)
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    users_service.add_UsersServicer_to_server(UsersService(), server)
+    server.add_insecure_port('127.0.0.1:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
 
 if __name__ == '__main__':
-  serve()
+    serve()
