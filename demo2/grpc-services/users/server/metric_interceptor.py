14def4805ac7599cfe911dde34b0f167418eb351
Amit Saha <amitsaha.in@gmail.com>
2017-10-03 15:45:00 +1100

    update

--- demo2/grpc-services/users/server/metric_interceptor.py
+++ demo2/grpc-services/users/server/metric_interceptor.py
@@ -1,6 +1,6 @@
 import time
 import grpc
-import sys
+import logging
 
 from grpc_interceptors import UnaryUnaryServerInterceptor, UnaryStreamServerInterceptor
 from datadog import DogStatsd
@@ -13,46 +13,36 @@ def push_to_statsd_histogram(metric, value, tags=[]):
     statsd.histogram(metric, value, tags)
 
 
-def push_to_statsd_increment(metric, value=1, tags=[]):
-    statsd.increment(metric, value, tags)
-
-
 def send_metrics(func):
     from functools import wraps
 
     @wraps(func)
     def wrapper(*args, **kw):
-        servicer_context = None
+        service_method = None
+        service_name = None
         if isinstance(args[4], grpc._server._Context):
             servicer_context = args[4]
+            # This gives us <service>/<method name>
+            service_method = servicer_context._rpc_event.request_call_details.method
+            service_name, method_name = str(service_method).rsplit('/')[1::]
         else:
-            raise Exception('MetricInterceptor cannot run. Expecting grpc._server.Context')
-        # This gives us <service>/<method name>
-        service_method = servicer_context._rpc_event.request_call_details.method
-        service_name, method_name = str(service_method).rsplit('/')[1::]
+            logging.warning('Cannot derive the service name and method')
         try:
             start_time = time.time()
             result = func(*args, **kw)
-        except:
+            result_status = 'success'
+        except Exception:
+            result_status = 'error'
+            raise
+        finally:
             resp_time = time.time() - start_time
             push_to_statsd_histogram(
                 REQUEST_LATENCY_METRIC_NAME,
-                resp_time,
-                ['service:{0}'.format(service_name),
-                 'method:{0}'.format(method_name),
-                 'status:error',
-                ]
-            )
-            raise
-        resp_time = time.time() - start_time
-        push_to_statsd_histogram(
-                REQUEST_LATENCY_METRIC_NAME,
-                resp_time,
-                ['service:{0}'.format(service_name),
-                 'method:{0}'.format(method_name),
-                 'status:success',
-                ]
-            )
+                resp_time, [
+                    'service:{0}'.format(service_name),
+                    'method:{0}'.format(method_name),
+                    'status:{0}'.format(result_status),
+                ])
         return result
     return wrapper
 
@@ -64,22 +54,10 @@ class MetricInterceptor(UnaryUnaryServerInterceptor, UnaryStreamServerIntercepto
 
     @send_metrics
     def intercept_unary_unary_handler(self, handler, method, request, servicer_context):
-        response = None
-        try:
-            response = handler(request, servicer_context)
-        except:
-            e = sys.exc_info()[0]
-            print(str(e))
-            raise
-        return response
+        return handler(request, servicer_context)
 
     @send_metrics
     def intercept_unary_stream_handler(self, handler, method, request, servicer_context):
-        try:
-            result = handler(request, servicer_context)
-            for response in result:
-                yield response
-        except:
-            e = sys.exc_info()[0]
-            print(str(e))
-            raise
+        result = handler(request, servicer_context)
+        for response in result:
+            yield response
