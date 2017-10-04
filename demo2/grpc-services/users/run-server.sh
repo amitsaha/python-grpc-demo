4f2c657bebecd769c2a8a1d0fcf691db08801b1a
Amit Saha <amitsaha.in@gmail.com>
2017-10-04 13:55:56 +1100

    move grpc_interecptors

--- demo2/grpc-services/users/run-server.sh
+++ demo2/grpc-services/users/run-server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-PYTHONPATH=../protos/gen-py python server/server.py
+# Add  grpc-interecptors to the PYTHONPATH
+# Add ../prots/gen-py to the PYTHONPATH
+PYTHONPATH=../protos/gen-py:../ python server/server.py
