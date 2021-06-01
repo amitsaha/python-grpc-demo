21f325d7bce43b3e4adf290e46a1f7ee894c3ff0
Amit Saha <amitsaha.in@gmail.com>
2021-06-01 22:07:19 +1000

    update

--- demo2/grpc-services/users/run-server.sh
+++ demo2/grpc-services/users/run-server.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# Add  grpc-interecptors to the PYTHONPATH
-# Add ../prots/gen-py to the PYTHONPATH
-PYTHONPATH=../protos/gen-py:../ python server/server.py
+PYTHONPATH=./gen-py:../ python server/server.py
